9d462dab9eb86ed41c702ab3e6bb18b532bd1d32
Alexandr Fedorov <fedorov2602@mail.ru>
2017-05-12 00:01:34 +0300

    Added support of file for storing values

--- include/b-tree.h
+++ include/b-tree.h
@@ -8,6 +8,7 @@
 #include <utility>
 #include <iostream>
 #include <exception>
+
 #include "cacher.h"
 #include "logger.h"
 
@@ -33,17 +34,17 @@ class Btree{
         void writeNode(std::fstream &f, Logger &logger, Cacher &cache);
         void delNode(std::fstream &f, Logger &logger, Cacher &cache, unsigned long long &nxt_space);
         void swap(Node &n);
-        void insertInLeaf(const Key &k, const Value &v);
+        void insertInLeaf(const Key &k, unsigned long long v);
         void eraseInLeaf(size_t pos);
         bool isLeaf();
-        void replaceKey(size_t pos, const Key &k, const Value &v);
-        void insert(const Key &k, const Value &v, unsigned long long son_offset);
+        void replaceKey(size_t pos, const Key &k, unsigned long long v);
+        void insert(const Key &k, unsigned long long v, unsigned long long son_offset);
 
-        const static size_t size = (2 * min_deg - 2) * sizeof(Value) + (2 * min_deg - 2) * sizeof(Key) + (2 * min_deg - 1) * sizeof(unsigned long long);
+        const static size_t size = (2 * min_deg - 2) * sizeof(unsigned long long) + (2 * min_deg - 2) * sizeof(Key) + (2 * min_deg - 1) * sizeof(unsigned long long);
         bool changed;
         unsigned long long offset;
         std::vector<Key> keys; //keys in order as in file
-        std::vector<Value> vals;
+        std::vector<unsigned long long> vals;
         std::vector<unsigned long long> refs; //references to next nodes (offsets)
 
      private:
@@ -55,29 +56,39 @@ class Btree{
     void del(unsigned long long offset, const Key &k, Node *par, size_t pos);
     bool find(unsigned long long offset, const Key &k, Value *v);
     void get(unsigned long long offset, const Key &l, const Key &r, std::vector<std::pair<Key, Value> > &res);
-    std::pair<Key, Value> delNext(unsigned long long offset, Node *par, size_t pos, const Key &k);
+    std::pair<Key, unsigned long long> delNext(unsigned long long offset, Node *par, size_t pos, const Key &k);
     void fix(Node &n, Node *par, size_t pos);
 
-    unsigned long long getNextSpace(std::fstream &f);
-    void changeOffset(unsigned long long offset, std::fstream &f);
+    Value getValue(unsigned long long offset);
+    char* getValueBin(unsigned long long offset);
+    void writeValue(unsigned long long offset, const Value &val, bool new_val = false);
+    void delValue(unsigned long long offset);
+
+    unsigned long long getNextSpace(std::fstream &f, unsigned long long &next_pos, bool is_value);
+    void changeOffset(unsigned long long offset, std::fstream &f, unsigned long long &next_pos, bool is_value);
 
     Btree(const Btree &b);
     void operator= (const Btree &b);
 
-    unsigned long long nxt_space;
+    unsigned long long nxt_space, nxt_space_vals;
+
     const size_t root = sizeof(unsigned long long);
+    const  size_t size_value = std::max(sizeof(Value), sizeof(unsigned long long));
+
     Logger logger;
-    std::fstream file;//, file_vals;
+    std::fstream file, file_vals;
     Cacher cache;
 };
 
 template <typename Key, typename Value, unsigned int t>
 Btree<Key, Value, t>::Btree():cache(Node::size){
-    file.open("btree.main", std::ios::in | std::ios::out | std::ios::binary); //remove
-    //file_vals.open("btree.vals", std::ios::in | std::ios::out | std::ios::binary);
-    logger.recoverTree(file);
+    file.open("btree.main", std::ios::in | std::ios::out | std::ios::binary);
+    file_vals.open("btree.vals", std::ios::in | std::ios::out | std::ios::binary);
+    logger.recoverTree(file, file_vals);
     nxt_space = 0;
-    if (getNextSpace(file) < Node::size + root){
+    nxt_space_vals = 0;
+
+    if (getNextSpace(file, nxt_space, false) < Node::size + root){
         file.seekp(0, std::ios_base::beg);
         char buf[Node::size + root];
         memset(buf, 0, Node::size + root);
@@ -86,32 +97,43 @@ Btree<Key, Value, t>::Btree():cache(Node::size){
         file.seekg(0, std::ios_base::beg);
         file.read((char*)&nxt_space, sizeof(unsigned long long));
     }
-    if (!file.good()){
+
+    if (getNextSpace(file_vals, nxt_space_vals, true) < sizeof(unsigned long long)){
+        file_vals.seekp(0, std::ios_base::beg);
+        char buf[sizeof(unsigned long long)];
+        memset(buf, 0, sizeof(unsigned long long));
+        file_vals.write(buf, sizeof(unsigned long long));
+    }else{
+        file_vals.seekg(0, std::ios_base::beg);
+        file_vals.read((char*)&nxt_space_vals, sizeof(unsigned long long));
+    }
+
+    if (!file.good() || !file_vals.good()){
         file.close();
+        file_vals.close();
         throw std::runtime_error("Error on opening file");
     }
 }
 
 template <typename Key, typename Value, unsigned int t>
-void Btree<Key, Value, t>::changeOffset(unsigned long long offset, std::fstream &f){
+void Btree<Key, Value, t>::changeOffset(unsigned long long offset, std::fstream &f, unsigned long long &next_pos, bool is_value){
     f.seekg(offset, std::ios_base::beg);
     unsigned long long pos;
     f.read((char*)&pos, sizeof(unsigned long long));
 
     char buf[sizeof(unsigned long long)];
     memcpy(buf, &offset, sizeof(unsigned long long));
-    logger.log(0, buf, sizeof(unsigned long long));
+    logger.log(0, buf, sizeof(unsigned long long), is_value);
     f.seekp(0, std::ios_base::beg);
     f.write((char*)&pos, sizeof(unsigned long long));
-    nxt_space = pos;
+    next_pos = pos;
 }
 
-
 template <typename Key, typename Value, unsigned int t>
-unsigned long long Btree<Key, Value, t>::getNextSpace(std::fstream &f){
-    if (nxt_space != 0){
-        unsigned long long offset = nxt_space;
-        changeOffset(nxt_space, f);
+unsigned long long Btree<Key, Value, t>::getNextSpace(std::fstream &f, unsigned long long &next_pos, bool is_value){
+    if (next_pos != 0){
+        unsigned long long offset = next_pos;
+        changeOffset(next_pos, f, next_pos, is_value);
         return offset;
     }
 
@@ -123,10 +145,28 @@ template <typename Key, typename Value, unsigned int t>
 bool Btree<Key, Value, t>::findElem(const Key &k, Value *v){
     logger.init();
     bool res = find(root, k, v);
-    if (!file.good())
+    file.flush();
+    file_vals.flush();
+    if (!file.good() || !file_vals.good())
         throw std::runtime_error("Error with file while findElem");
     logger.finish();
     return res;
+}
+
+template <typename Key, typename Value, unsigned int t>
+Value Btree<Key, Value, t>::getValue(unsigned long long offset){
+    Value v;
+    file_vals.seekg(offset, std::ios_base::beg);
+    file_vals.read((char*)&v, sizeof(Value));
+    return v;
+}
+
+template <typename Key, typename Value, unsigned int t>
+char* Btree<Key, Value, t>::getValueBin(unsigned long long offset){
+    char *buf = new char[size_value];
+    file_vals.seekg(offset, std::ios_base::beg);
+    file_vals.read(buf, size_value);
+    return buf;
 }
 
 template <typename Key, typename Value, unsigned int t>
@@ -135,7 +175,7 @@ bool Btree<Key, Value, t>::find(unsigned long long offset, const Key &k, Value *
     typename std::vector<Key>::iterator it = lower_bound(n.keys.begin(), n.keys.end(), k);
     size_t pos = it - n.keys.begin();
     if (it != n.keys.end() && *it == k){
-        *v = n.vals[pos];
+        *v = getValue(n.vals[pos]);
         return true;
     }else
     if (!n.isLeaf()){
@@ -148,7 +188,9 @@ template <typename Key, typename Value, unsigned int t>
 void Btree<Key, Value, t>::getElems(const Key &l, const Key &r, std::vector<std::pair<Key, Value> > &res){
     logger.init();
     get(root, l, r, res);
-    if (!file.good())
+    file.flush();
+    file_vals.flush();
+    if (!file.good() || !file_vals.good())
         throw std::runtime_error("Error with file while getElems");
     logger.finish();
 }
@@ -163,14 +205,16 @@ void Btree<Key, Value, t>::get(unsigned long long offset, const Key &l, const Ke
     for (size_t i = lpos; i <= rpos; i++)
         get(n.refs[i], l, r, res);
     for (size_t i = lpos; i < rpos; i++)
-        res.emplace_back(n.keys[i], n.vals[i]);
+        res.emplace_back(n.keys[i], getValue(n.vals[i]));
 }
 
 template <typename Key, typename Value, unsigned int min_deg>
 void Btree<Key, Value, min_deg>::addElem(const Key &k, const Value &v){
     logger.init();
     add(root, k, v, NULL); //for root parent = NULL
-    if (!file.good())
+    file.flush();
+    file_vals.flush();
+    if (!file.good() || !file_vals.good())
         throw std::runtime_error("Error with file while addElem");
     logger.finish();
 }
@@ -178,25 +222,26 @@ void Btree<Key, Value, min_deg>::addElem(const Key &k, const Value &v){
 template <typename Key, typename Value, unsigned int min_deg>
 void Btree<Key, Value, min_deg>::add(unsigned long long offset, const Key &k, const Value &v, Node *par){
     Node n(file, offset, cache);
-    //std::cout << n.offset << " " << n.keys.size() << " " << n.refs.size() << std::endl;
     typename std::vector<Key>::iterator it = lower_bound(n.keys.begin(), n.keys.end(), k);
     size_t pos = it - n.keys.begin();
     if (it != n.keys.end() && *it == k){
-        //n.update(pos, v);
-        n.changed = true;
-        n.vals[pos] = v;
+        writeValue(n.vals[pos], v);
     }else{
-        if (n.isLeaf()) //leaf
-            n.insertInLeaf(k, v);
-        else{ //not leaf
+        if (n.isLeaf()){ //leaf
+            bool new_val = (nxt_space_vals == 0);
+            unsigned long long place = getNextSpace(file_vals, nxt_space_vals, true);
+            writeValue(place, v, new_val);
+            n.insertInLeaf(k, place);
+        }else{ //not leaf
             add(n.refs[pos], k, v, &n);
         }
     }
+
     if (n.keys.size() == 2 * min_deg - 1){
         Node new_node;
         size_t mid = min_deg;
         n.changed = new_node.changed = true;
-        new_node.offset = getNextSpace(file);
+        new_node.offset = getNextSpace(file, nxt_space, false);
         new_node.vals.insert(new_node.vals.begin(), n.vals.begin() + mid, n.vals.end());
         new_node.keys.insert(new_node.keys.begin(), n.keys.begin() + mid, n.keys.end());
         if (!n.isLeaf())
@@ -206,7 +251,7 @@ void Btree<Key, Value, min_deg>::add(unsigned long long offset, const Key &k, co
 
         if (par == NULL){
             Node new_root;
-            new_root.offset = getNextSpace(file);
+            new_root.offset = getNextSpace(file, nxt_space, false);
             new_root.changed = true;
             new_root.swap(n);
             new_root.refs.emplace_back(n.offset);
@@ -312,7 +357,9 @@ template <typename Key, typename Value, unsigned int min_deg>
 void Btree<Key, Value, min_deg>::delElem(const Key &k){
     logger.init();
     del(root, k, NULL, 0);
-    if (!file.good())
+    file.flush();
+    file_vals.flush();
+    if (!file.good() || !file_vals.good())
         throw std::runtime_error("Error with file while delElem");
     logger.finish();
 }
@@ -323,17 +370,18 @@ void Btree<Key, Value, min_deg>::del(unsigned long long offset, const Key &k, No
     typename std::vector<Key>::iterator it = lower_bound(n.keys.begin(), n.keys.end(), k);
     size_t pos = it - n.keys.begin();
     if (n.isLeaf()){
-        if (it == n.keys.end() || *it != k){
+        if (it == n.keys.end() || *it != k)
             return;
-        }
+
+        delValue(n.vals[pos]);
         n.eraseInLeaf(pos);
     }else{
         if (it != n.keys.end() && *it == k){
-            std::pair<Key, Value> next_key = delNext(n.refs[pos + 1], &n, pos + 1, k);
+            delValue(n.vals[pos]);
+            std::pair<Key, unsigned long long> next_key = delNext(n.refs[pos + 1], &n, pos + 1, k);
             it = std::find(n.keys.begin(), n.keys.end(), k);
             if (it != n.keys.end())
                 n.replaceKey(it - n.keys.begin(), next_key.first, next_key.second);
-            //n.replace_key(pos, next_key.first, next_key.second);
         }else
             del(n.refs[pos], k, &n, pos);
     }
@@ -344,9 +392,9 @@ void Btree<Key, Value, min_deg>::del(unsigned long long offset, const Key &k, No
 
 
 template <typename Key, typename Value, unsigned int min_deg>
-std::pair<Key, Value> Btree<Key, Value, min_deg>::delNext(unsigned long long offset, Node *par, size_t from, const Key &k){
+std::pair<Key, unsigned long long> Btree<Key, Value, min_deg>::delNext(unsigned long long offset, Node *par, size_t from, const Key &k){
     Node n(file, offset, cache);
-    std::pair<Key, Value> res;
+    std::pair<Key, unsigned long long> res;
     if (n.isLeaf()){
         res = std::make_pair(n.keys[0], n.vals[0]);
         n.eraseInLeaf(0);
@@ -368,14 +416,14 @@ template <typename Key, typename Value, unsigned int min_deg>
 void Btree<Key, Value, min_deg>::Node::eraseInLeaf(size_t pos){
     changed = true;
     keys.erase(keys.begin() + pos);
-    vals.erase(vals.begin() + pos); // do
+    vals.erase(vals.begin() + pos);
 }
 
 template <typename Key, typename Value, unsigned int min_deg>
-void Btree<Key, Value, min_deg>::Node::replaceKey(size_t pos, const Key &k, const Value &v){
+void Btree<Key, Value, min_deg>::Node::replaceKey(size_t pos, const Key &k, unsigned long long v){
     changed = true;
     keys[pos] = k;
-    vals[pos] = v; // to do
+    vals[pos] = v;
 }
 
 template <typename Key, typename Value, unsigned int min_deg>
@@ -416,11 +464,11 @@ char* Btree<Key, Value, min_deg>::Node::getBinary(){
 
     for (size_t i = 0; i < (2 * min_deg - 2); i++)
         if (i < vals.size()){
-            memcpy(buf + pos, &vals[i], sizeof(Value));
-            pos += sizeof(Value);
+            memcpy(buf + pos, &vals[i], sizeof(unsigned long long));
+            pos += sizeof(unsigned long long);
         }else{
-            memset(buf + pos, 0, sizeof(Value));
-            pos += sizeof(Value);
+            memset(buf + pos, 0, sizeof(unsigned long long));
+            pos += sizeof(unsigned long long);
         }
     return buf;
 }
@@ -431,7 +479,7 @@ void Btree<Key, Value, min_deg>::Node::writeNode(std::fstream &f, Logger &logger
         return;
     char *bin = getBinary();
     cache.update(offset, bin);
-    logger.log(offset, old, size);
+    logger.log(offset, old, size, false);
     f.seekp(offset, std::ios_base::beg);
     f.write(bin, size);
     delete [] bin;
@@ -444,7 +492,7 @@ void Btree<Key, Value, min_deg>::Node::swap(Node &n){
 }
 
 template <typename Key, typename Value, unsigned int min_deg>
-void Btree<Key, Value, min_deg>::Node::insertInLeaf(const Key &k, const Value &v){
+void Btree<Key, Value, min_deg>::Node::insertInLeaf(const Key &k, unsigned long long v){
     changed = true;
     typename std::vector<Key>::iterator it = lower_bound(keys.begin(), keys.end(), k);
     vals.insert(vals.begin() + (it - keys.begin()), v);
@@ -452,7 +500,7 @@ void Btree<Key, Value, min_deg>::Node::insertInLeaf(const Key &k, const Value &v
 }
 
 template <typename Key, typename Value, unsigned int min_deg>
-void Btree<Key, Value, min_deg>::Node::insert(const Key &k, const Value &v, unsigned long long son_offset){
+void Btree<Key, Value, min_deg>::Node::insert(const Key &k, unsigned long long v, unsigned long long son_offset){
     changed = true;
     typename std::vector<Key>::iterator it = lower_bound(keys.begin(), keys.end(), k);
     vals.insert(vals.begin() + (it - keys.begin()), v);
@@ -467,13 +515,13 @@ void Btree<Key, Value, min_deg>::Node::delNode(std::fstream &f, Logger &logger, 
     memset(buf, 0, size);
     memcpy(buf, &nxt_space, sizeof(unsigned long long));
     cache.update(offset, buf);
-    logger.log(offset, old, size);
+    logger.log(offset, old, size, false);
     f.seekp(offset, std::ios_base::beg);
     f.write(buf, size);
 
     char tmp[sizeof(unsigned long long)];
     memcpy(tmp, &nxt_space, sizeof(unsigned long long));
-    logger.log(0, tmp, sizeof(unsigned long long));
+    logger.log(0, tmp, sizeof(unsigned long long), false);
     memcpy(tmp, &offset, sizeof(unsigned long long));
     f.seekp(0, std::ios_base::beg);
     f.write(tmp, sizeof(unsigned long long));
@@ -505,19 +553,61 @@ Btree<Key, Value, min_deg>::Node::Node(std::fstream &f, unsigned long long ps, C
         if (i < refs.size())
             keys.emplace_back(k);
     }
-    Value v;
+    unsigned long long v;
     for (size_t i = 1; i < mx; i++){
-        memcpy(&v, old + pos, sizeof(Value));
-        pos += sizeof(Value);
+        memcpy(&v, old + pos, sizeof(unsigned long long));
+        pos += sizeof(unsigned long long);
         if (i < refs.size())
             vals.emplace_back(v);
     }
-    if (!refs.empty() && refs[0] == 1)
+    if (!refs.empty() && refs[0] == 1) // is leaf
         refs.clear();
+}
+
+template <typename Key, typename Value, unsigned int min_deg>
+void Btree<Key, Value, min_deg>::writeValue(unsigned long long offset, const Value &val, bool new_val){
+    char buf[size_value];
+    memset(buf, 0, size_value);
+    if (!new_val){
+        char *old = getValueBin(offset);
+        memcpy(buf, old, size_value);
+        delete[] old;
+    }
+    logger.log(offset, buf, size_value, true);
+
+    memset(buf, 0, size_value);
+    memcpy(buf, &val, sizeof(Value));
+    file_vals.seekp(offset, std::ios_base::beg);
+    file_vals.write(buf, size_value);
+}
+
+
+template <typename Key, typename Value, unsigned int min_deg>
+void Btree<Key, Value, min_deg>::delValue(unsigned long long offset){
+    char buf[size_value], old[size_value];
+    memset(buf, 0, size_value);
+    memcpy(buf, &nxt_space_vals, sizeof(unsigned long long));
+
+    memset(old, 0, size_value);
+    char *last = getValueBin(offset);
+    memcpy(old, last, size_value);
+    delete[] last;
+    logger.log(offset, old, size_value, true);
+    file_vals.seekp(offset, std::ios_base::beg);
+    file_vals.write(buf, size_value);
+
+    char tmp[sizeof(unsigned long long)];
+    memcpy(tmp, &nxt_space_vals, sizeof(unsigned long long));
+    logger.log(0, tmp, sizeof(unsigned long long), true);
+    memcpy(tmp, &offset, sizeof(unsigned long long));
+    file_vals.seekp(0, std::ios_base::beg);
+    file_vals.write(tmp, sizeof(unsigned long long));
+    nxt_space_vals = offset;
 }
 
 template <typename Key, typename Value, unsigned int min_deg>
 Btree<Key, Value, min_deg>::Node::Node(){
     memset(old, 0, size);
 }
+
 #endif
